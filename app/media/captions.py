1acd841ea9b52804bcf06c0cab771f6167c7ae06
AbdullahArif17 <abdullaharif893@gmail.com>
2026-09-01 03:07:01 +0500

    feat: Update ASS styles for improved readability and alignment in captions

--- app/media/captions.py
+++ app/media/captions.py
@@ -243,8 +243,8 @@ PlayResY: 1920
 
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
-Style: Default,Montserrat,90,&H0000FFFF,&H00FFFFFF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,10,0,5,40,40,0,1
-Style: Emphasis,Montserrat,96,&H0000FF00,&H00FFFFFF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,10,0,5,40,40,0,1
+Style: Default,Montserrat,90,&H0000FFFF,&H00FFFFFF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,10,0,2,40,40,350,1
+Style: Emphasis,Montserrat,96,&H0000FF00,&H00FFFFFF,&H00000000,&H00000000,-1,0,0,0,100,100,0,0,1,10,0,2,40,40,350,1
 
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
